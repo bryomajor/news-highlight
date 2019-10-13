32082ed3f8dac13e7d2f9a77088e889b136e4d43
Brian Otieno <bryomajor@gmail.com>
2019-10-13 17:33:49 +0300

    Call the news api key using the environ.get method

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ class Config:
     EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize={}&apiKey={}'
     EVERYTHING_SEARCH_URL = 'https://newsapi.org/v2/everything?q={}&pageSize={}&apiKey={}'
 
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
     # title = "Home - News From Various News Sources"
 
     # return render_template('index.html')
