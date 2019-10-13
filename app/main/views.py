4e0676be89c2cb43ffdf4d4167db140c4d34f236
Brian Otieno <bryomajor@gmail.com>
2019-10-13 18:43:41 +0300

    Create a view function that returns the headlines articles

--- app/main/views.py
+++ app/main/views.py
@@ -30,3 +30,14 @@ def articles(source_id, per_page):
     title = f'{source_id} | All Articles'
 
     return render_template('articles.html', title = title, name = source_id, news = news_source)
+
+
+@main.route('/topheadlines&<int:per_page>')
+def headlines(per_page):
+    '''
+    Function that returns top headlines articles
+    '''
+    topheadlines_news = topheadlines(per_page)
+    title = 'Top headlines'
+
+    return render_template('topheadlines.html', title = title, name = 'Top Headlines', news = topheadlines_news)
