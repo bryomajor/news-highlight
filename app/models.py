ae3f94a51fdf151a8c52850f8aad138a6574ec32
Brian Otieno <bryomajor@gmail.com>
2019-10-13 16:07:01 +0300

    Create Sources class

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class Sources:
+    '''
+    Sources class
+    '''
+    def __init__(self, id, name, description, url, category, country):
+        '''
+        Function that initiates the source class
+        '''
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
