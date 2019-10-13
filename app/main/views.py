0494c521f743f1f2b00d865e58f7ca943f7f2140
Brian Otieno <bryomajor@gmail.com>
2019-10-13 20:47:03 +0300

    Add a function that shows results of a search request

--- app/main/views.py
+++ app/main/views.py
@@ -56,4 +56,19 @@ def all_news(per_page):
     if search_articles:
         return redirect(url_for('main.search', topic = search_articles))
     else:
-        return render_template('everything.html', title = title, name = 'All News', news = everything_news)
+        return render_template('topheadlines.html', title = title, name = 'All News', news = everything_news)
+
+
+@main.route('/search/<topic>')
+def search(topic):
+    '''
+    function that returns the results of search request
+    '''
+    limit = 40
+    search_name = topic.split(" ")
+    search_name_format = "+".join(search_name)
+    search_every = search_everything(limit,search_name_format)
+
+    title = '{search_name_format} Results'
+
+    return render_template('search.html',title=title,news = search_every)
