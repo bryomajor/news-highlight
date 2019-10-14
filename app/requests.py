19ebf95ed18a754e8c8aa2a7992b4564db6b0994
Brian Otieno <bryomajor@gmail.com>
2019-10-14 17:17:28 +0300

    A little clean up.

--- app/requests.py
+++ app/requests.py
@@ -2,7 +2,7 @@ import urllib.request, json
 from .models import Sources, Articles
 from datetime import datetime
 
-api_key = None
+apiKey = None
 sources_url = None
 articles_url = None
 topheadlines_url = None
@@ -10,20 +10,18 @@ everything_url = None
 everything_search_url = None
 
 def configure_request(app):
-    global api_key, sources_url, articles_url, topheadlines_url, everything_url, everything_search_url
-    api_key = app.config['NEWS_API_KEY']
+    global apiKey, sources_url, articles_url, topheadlines_url
+    apiKey = app.config['NEWS_API_KEY']
     sources_url = app.config['SOURCES_BASE_URL']
-    articles_url = app.config['EVERYTHING_SOURCE_BASE_URL']
+    articles_url = app.config['ARTICLE_SOURCE_BASE_URL']
     topheadlines_url = app.config['TOP_HEADLINES_BASE_URL']
-    everything_url = app.config['EVERYTHING_BASE_URL']
-    everything_search_url = app.config['EVERYTHING_SEARCH_URL']
 
 
 def get_sources(category):
     '''
     Function that gets the json repsonse to out url request
     '''
-    get_sources_url = sources_url.format(category, api_key)
+    get_sources_url = sources_url.format(category, apiKey)
 
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
@@ -52,17 +50,17 @@ def process_results(sources_list):
         country = source.get('country')
 
         if url:
-            source_object = Sources(id, name, description, url, category. country)
+            source_object = Sources(id, name, description, url, category, country)
             sources_results.append(source_object)
 
     return sources_results
 
 
-def get_articles(source_id, limit):
+def get_articles(source_id, article):
     '''
     Function that gets articles based on the source id
     '''
-    get_article_location_url = articles_url.format(source_id, limit, api_key)
+    get_article_location_url = articles_url.format(source_id, article, apiKey)
 
     with urllib.request.urlopen(get_article_location_url) as url:
         articles_location_data = url.read()
@@ -99,11 +97,11 @@ def process_articles(my_articles):
     return article_location_list
 
 
-def topheadlines(limit):
+def topheadlines(category):
     '''
     Function that gets articles based on the source id
     '''
-    get_topheadlines_url = topheadlines.url.format(limit, api_key)
+    get_topheadlines_url = topheadlines_url.format(apiKey)
 
     with urllib.request.urlopen(get_topheadlines_url) as url:
         topheadlines_data = url.read()
@@ -115,39 +113,3 @@ def topheadlines(limit):
             topheadlines_results = process_articles(topheadlines_response['articles'])
 
     return topheadlines_results
-
-
-def everything(limit):
-    '''
-    Function that gets articles based on the source id
-    '''
-    get_everything_url = everything_url.format(limit, api_key)
-
-    with urllib.request.urlopen(get_everything_url) as url:
-        everything_data = url.read()
-        everything_response = json.loads(everything_data)
-
-        everything_results = None
-
-        if everything_response['articles']:
-            everything_results = process_articles(everything_response['articles'])
-
-    return everything_results
-
-
-def search_everything(limit, query):
-    '''
-    Function that looks for articles based on top headlines
-    '''
-    search_everything_url = everything_search_url.format(query, limit, api_key)
-
-    with urllib.request.urlopen(search_everything_url) as url:
-        search_everything_data = url.read()
-        search_everything_response = json.loads(search_everything_data)
-
-        search_everything_results = []
-
-        if search_everything_response['articles']:
-            search_everything_results = process_articles(search_everything_response['articles'])
-            
-    return search_everything_results
